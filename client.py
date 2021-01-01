0ba2c7009c06d383e8e9fd15d8bfa83eea0db2ec
Caushik Subramaniam C <caushik11@gmail.com>
2021-01-01 18:12:00 +0530

    initial commit

--- client.py
+++ client.py
@@ -1,10 +1,46 @@
 from store import Store
 import json
-import time
+import random
+from getpass import getpass
 
-# store = Store()
-# id = "12345"
-# details = {"username": "daniel", "password": "dany12345", "details": {"name": "Daniel", "dob": "9-9-1999"}}
-# store.create(id, details)
-# print(store.read(id))
-# store.delete(id)
+class App:
+    def __init__(self):
+        self.store = Store()
+
+    def signup(self, username, password, name):
+        id = username
+        try:
+            self.store.create(id, {"name":name, "password": password})
+            return 1
+        except KeyError:
+            return 0
+
+    def login(self, username, password):
+        try:
+            user = json.loads(self.store.read(username))
+            if user["password"] == password:
+                return 1
+        except KeyError:
+            return 0
+
+app = App()
+print("Simple Auth App")
+case = input("\na. Sign up / b. Log in [a/B]? ")
+
+if case == 'a':
+    name = input("Name: ")
+    username = input("Username: ")
+    password = getpass("Password: ")
+
+    if app.signup(username, password, name) == 1:
+        print("Sign up success!")
+    else:
+        print("Sign up failed!")
+
+elif case == 2 or case == "":
+    username = input("Username: ")
+    password = getpass("Password: ")
+    if app.login(username, password) == 1:
+        print("Log in success!")
+    else:
+        print("Log in failed!")
